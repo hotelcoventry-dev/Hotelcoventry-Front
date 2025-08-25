0e1500a76c1110d72b51f570546b4ea1249b3e81
Ivan Trullenque <169075084+ivanTrullenque@users.noreply.github.com>
2025-08-25 11:36:54 -0300

    modificacion en sidebar

---
+++ src/app/(dashboard)/admin/reservas/components/ReservasComponent.tsx
@@ -0,0 +1,9 @@
+'use client'
+export const ReservasComponent = () => {
+  return (
+    <div>
+      <h2>Lista de Reservas</h2>
+      {/* Aquí puedes agregar la tabla o lista de reservas */}
+    </div>
+  )
+}
